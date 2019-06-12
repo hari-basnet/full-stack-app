a1e3ca71f18fefc637e1d19870a63c5978da5625
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-06-12 10:16:11 +0300

    added handlechange method

--- client/src/components/AddStudent.js
+++ client/src/components/AddStudent.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class AddStudent extends Component {
+    state = {
+        name: '',
+        country: '',
+        age: '',
+        bio: '',
+
+    }
+
+    handleChange = (e) =>{
+        const {name, value} = e.target;
+        this.setState({
+            [name] : value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post(url, this.state)
+        .then(response => {})
+        .catch(error => {})
+
+    }
     render() {
         return (
-            <form>
+            <form onSubmit ={this.handleSubmit}>
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" class="form-control" placeholder="Enter Name" />
+                    <input 
+                    type="text" 
+                    class="form-control" 
+                    name="name" 
+                    placeholder="Enter Name" 
+                    value ={this.state.name} 
+                    onChange={this.handleChange} />
                 </div>
                 <div class="form-group">
                     <label for="country">Country</label>
-                    <input type="text" class="form-control" placeholder="Enter Country" />
+                    <input 
+                    type="text" 
+                    class="form-control" 
+                    name="country" 
+                    placeholder="Enter Country" 
+                    value ={this.state.country} 
+                    onChange={this.handleChange}/>
                 </div>
                 <div class="form-group">
                     <label for="age">Age</label>
-                    <input type="text" class="form-control" placeholder="Enter age" />
+                    <input 
+                    type="text" 
+                    class="form-control" 
+                    name="age" 
+                    placeholder="Enter age" 
+                    value ={this.state.age} 
+                    onChange={this.handleChange}/>
                 </div>
 
                 <div class="form-group">
                     <label for="bio">Bio</label>
-                    <textarea name="bio" class="form-control" />
+                    <textarea 
+                    name="bio" 
+                    class="form-control" 
+                    value ={this.state.bio} 
+                    onChange={this.handleChange}/>
                 </div>
                 <button class="btn btn-primary" type="submit">Add Student</button>
 
